473e4a41b5a53429a90c8c747da57302b3083b01
Hammed <Hammed74@gmail.com>
2026-02-15 17:13:37 -0500

    Initial Commit

---
+++ CoveFrontend/CoveFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoveFrontendApp.swift
+//  CoveFrontend
+//
+//  Created by Med Sylla on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct CoveFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
